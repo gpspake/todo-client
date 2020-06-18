6ff79c2e892cae9e2a1c2c2a6e238f52c1953be3
George Spake <gpspake@gmail.com>
2020-06-17 23:39:48 -0500

    remove placeholder text

--- src/components/TodoInput.tsx
+++ src/components/TodoInput.tsx
@@ -46,7 +46,6 @@ export const TodoInput = (props: ITodoInputProps) => {
           className="py-3 px-4 text-gray-800 border-gray-300 border outline-none placeholder-gray-500 focus:bg-gray-100 w-full"
           type="text"
           name="todo"
-          placeholder={props.placeHolder || 'Walk the dog'}
           value={name}
           onChange={onChange}
           onKeyDown={handleKeyDown}
