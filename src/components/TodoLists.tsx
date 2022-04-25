7db410d2ecc4cf105b1ec628119cdf1158613505
George Spake <george@georgespake.com>
2022-04-24 22:38:44 -0600

    refactor todo lists component
    extract authenticated todo lists component to prevent fetching before authenticating

--- src/components/TodoLists.tsx
+++ src/components/TodoLists.tsx
@@ -8,33 +8,14 @@ import {TodoListLink} from "./TodoListLink";
 import { Loading } from './Loading';
 import { useAuth0 } from '@auth0/auth0-react';
 
-export const TodoLists = () => {
-
-  const {
-    status: fetchTodoListsStatus,
-    data: todoLists,
-    // error: fetchTodoListsError
-  } = useFetchTodoLists()
-
-  const { isAuthenticated, isLoading } = useAuth0()
-
+const AuthenticatedTodoLists = () => {
+  const { data: todoLists } = useFetchTodoLists()
   const  { mutateAsync: deleteTodoListMutation} = useDeleteTodoList()
-
   return (
     <>
-      {isLoading || (isAuthenticated && !todoLists) && <Loading />}
-
-      {!isLoading && !isAuthenticated && (
-        <div className="text-center mt-8">
-          <p className="text-teal-500">Offline demo coming soon</p>
-          <p className="text-teal-500">Please log in</p>
-        </div>
-      )}
-
-      {!isLoading && isAuthenticated && (
-        <>
-          {todoLists && (
-            <Link className="block flex align-items-center mt-8 " to="/todo/new">
+      {!todoLists && <Loading />}
+      {todoLists && (
+        <Link className="block flex align-items-center mt-8 " to="/todo/new">
               <span className="fa-layers fa-fw fa-3x block m-auto group">
                 <FontAwesomeIcon
                   icon={faCircle}
@@ -42,28 +23,40 @@ export const TodoLists = () => {
                 />
                 <FontAwesomeIcon icon={faPlus} inverse transform="shrink-8" />
               </span>
-            </Link>
-          )}
+        </Link>
+      )}
+      {todoLists && !!todoLists.length && (
+        <div className="max-w-sm rounded overflow-hidden shadow-lg bg-white mx-auto mt-8 border border-gray-300">
+          <ul>
+            {todoLists.map((list, index) => (
+              <TodoListLink
+                key={list.id}
+                todoList={list}
+                deleteTodoList={deleteTodoListMutation}
+                className={classNames({ 'border-t': !!index })}
+              />
+            ))}
+          </ul>
+        </div>
+      )}
+      {todoLists && !todoLists.length && (
+        <p className="mt-6 text-center text-gray-500">Add a list</p>
+      )}
+    </>
+  )
+}
 
-          {todoLists && !!todoLists.length && (
-            <div className="max-w-sm rounded overflow-hidden shadow-lg bg-white mx-auto mt-8 border border-gray-300">
-              <ul>
-                {todoLists.map((list, index) => (
-                  <TodoListLink
-                    key={list.id}
-                    todoList={list}
-                    deleteTodoList={deleteTodoListMutation}
-                    className={classNames({ 'border-t': !!index })}
-                  />
-                ))}
-              </ul>
-            </div>
-          )}
-
-          {todoLists && !todoLists.length && (
-            <p className="mt-6 text-center text-gray-500">Add a list</p>
-          )}
-        </>
+export const TodoLists = () => {
+  const { isAuthenticated, isLoading } = useAuth0()
+  return (
+    <>
+      {isLoading && <Loading />}
+      {!isLoading && isAuthenticated && <AuthenticatedTodoLists />}
+      {!isLoading && !isAuthenticated && (
+        <div className="text-center mt-8 text-teal-500">
+          <p>Offline demo coming soon</p>
+          <p>Please log in</p>
+        </div>
       )}
     </>
   )
