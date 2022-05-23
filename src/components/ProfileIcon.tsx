8cfe3068d3db35db58411e891d01490ba84b5e80
George Spake <george@georgespake.com>
2022-05-23 09:22:03 -0500

    check for user before rendering to fix ts error

--- src/components/ProfileIcon.tsx
+++ src/components/ProfileIcon.tsx
@@ -5,7 +5,7 @@ import { useAuth0 } from '@auth0/auth0-react';
 export const ProfileIcon: React.FC = ({ children }) => {
   const [open, setOpen] = useState(false)
   const { user } = useAuth0()
-  
+
   return (
     <>
       <button
@@ -13,9 +13,9 @@ export const ProfileIcon: React.FC = ({ children }) => {
         onClick={() => {setOpen(true)}}
         className="transition-all duration-200 ease-in-out  inline-block text-sm leading-none rounded-full text-white hover:border-teal-600 border-2 border-transparent hover:text-teal-500 lg:mt-0"
       >
-        <img className="h-10 rounded-full" src={user.picture} alt="" />
+        {user && <img className="h-10 rounded-full" src={user.picture} alt="" />}
       </button>
-      
+
       {open && (
         <ProfileInformation onClose={() => {setOpen(false)}}>
           {children}
